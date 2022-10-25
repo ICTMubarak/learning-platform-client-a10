e7b6916884e67061749faa7f0b2659b9dfa02787
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-25 09:21:45 +0600

    Added Register page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
     return (
         <div>
                 <div className="navbar bg-primary text-primary-content">
+            
                      <Link to='/' className="btn btn-ghost normal-case text-xl">Learn With Mubarak</Link>
                      <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
                      <Link className="btn btn-ghost normal-case text-xl" to='/login'>Log in</Link>
