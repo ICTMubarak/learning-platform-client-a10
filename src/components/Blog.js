1cf716469e3d46b923993b8696e49702f6cdfab8
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-26 18:50:17 +0600

    Updated Blog and FQA page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -19,12 +19,23 @@ const Blog = () => {
                 }
               
            </div>
-           
-           
        </div>
             
             <div class="col-span-5 border-2">
-                <h1>This is blog site</h1>
+                <h1>Welcome to our Blog site</h1>
+                <br />
+                <h5>Question No-1 what is `cors`?</h5>
+                <p>Answer: CORS stands for Cross-Origin Resource Sharing . It allows us to relax the security applied to an API. This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API.</p>
+                <br />
+                <h5>Question No-2 Why are you using `firebase`? What other options do you have to implement authentication?</h5>
+                <p>Answer: Google Analytics for Firebase allows you to track your users' journey through realtime and custom reporting. As mentioned previously, Firebase supports IOS, Android, Web, and Unity products, allowing you to track your users across a wide range of devices. We have some options to implement authentication. As like email, password. We can also use Parse, Back4App, AWS Amplify, Kuzzle, Couchbase, NativeScript, RxDB, Flutter, LoopBack, SashiDo.</p>
+                <br />
+                <h5>Question No-3 How does the private route work?</h5>
+                <p>Answer: The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).</p>
+                <br />
+                <h5>Question No-4 What is Node? How does Node work?</h5>
+                <p>Answer: Node. js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node. js wastes no time or resources on waiting for I/O requests to return.</p>
+                <br />
             </div>
         </div>
     );
--- src/components/FQA.js
+++ src/components/FQA.js
@@ -20,7 +20,21 @@ const FQA = () => {
        </div>
             
             <div class="col-span-5 border-2">
-                <h1>This is FQA site</h1>
+                <h1>FQA</h1>
+                <br />
+
+                <h5>1. Why we will use this site?</h5>
+                <p>Ans: It is very easy to access and to increase your knowledge.</p>
+                <h5>2. How many topics added in your site?</h5>
+                <p>Ans: Currently we added 6 topic in our site. HTML, CSS, Bootstrup. Tailwind, JavaScript, and React</p>
+                <h5>3. Is it free to access your site?</h5>
+                <p>Ans: Yeah! It is totally free for you. But if you access our tutorial you must registerd and logind</p>
+                <h5>4. Which Language used to build your website?</h5>
+                <p>Ans: We used HTML, CSS, JS and React to build this site.</p>
+                <h5>5. What is your future plan about your website?</h5>
+                <p>Ans: In future we added mutch more topics in our site. </p>
+ 
+
             </div>
         </div>
     );
